2d8486e8b8212348a244b113483022912a4a44c2
AlexFreik <alexfreik5@gmail.com>
2024-06-28 20:25:27 +0530

    update

---
+++ src/Calendar.js
@@ -0,0 +1,19 @@
+function Calendar() {
+    return (
+        <div id="calendar">
+          <div className="m-auto my-20 flex w-3/4 flex-col gap-8">
+            <div className="skeleton h-10 w-full"></div>
+            <div className="skeleton h-10 w-full"></div>
+            <div className="skeleton h-10 w-full"></div>
+            <div className="skeleton h-10 w-full"></div>
+            <div className="skeleton h-10 w-full"></div>
+            <div className="skeleton h-10 w-full"></div>
+            <div className="skeleton h-10 w-full"></div>
+            <div className="skeleton h-10 w-full"></div>
+            <div className="skeleton h-10 w-full"></div>
+          </div>
+        </div>
+    );
+}
+
+export default Calendar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Calendar from './Calendar';
+import Sidebar from './Sidebar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+        <Calendar />
+        <Sidebar />
+    </React.StrictMode >,
 );
